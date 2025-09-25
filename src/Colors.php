68200560e5994a2ab23b8a634c2925d404e8665e
Imp Bugsoff <imp@bugsoff.ru>
2025-09-25 02:03:53 +0000

    fix reset colors

--- src/Colors.php
+++ src/Colors.php
@@ -5,6 +5,8 @@ namespace App;
 
 class Colors
 {
+    public const RESET_ALL   = "\033[0m";     # Text Reset
+
     public const BLACK       = "\033[0;30m";  # Black
     public const RED         = "\033[0;31m";  # Red
     public const GREEN       = "\033[0;32m";  # Green
@@ -13,8 +15,8 @@ class Colors
     public const PURPLE      = "\033[0;35m";  # Purple
     public const CYAN        = "\033[0;36m";  # Cyan
     public const WHITE       = "\033[0;37m";  # White
-    public const RESET       = "\033[0m";     # Text Reset
-
+    public const RESET       = "\033[39m";    # Reset Text Color
+    
     public const BOLD_BLACK  = "\033[1;30m";  # Black
     public const BOLD_RED    = "\033[1;31m";  # Red
     public const BOLD_GREEN  = "\033[1;32m";  # Green
@@ -32,5 +34,5 @@ class Colors
     public const BG_PURPLE   = "\033[45m";    # Purple
     public const BG_CYAN     = "\033[46m";    # Cyan
     public const BG_WHITE    = "\033[47m";    # White
-
+    public const BG_RESET    = "\033[49m";    # Reset BG
 }
--- src/functions.php
+++ src/functions.php
@@ -2,13 +2,12 @@
 
 use App\Colors;
 
-function cprintf($color = Colors::RESET, $format = '', ...$args)
+function cprintf($color = Colors::RESET_ALL, $format = '', ...$args)
 {
     echo 
         new DateTime()->format('Y-m-d H:i:s.u> '),
         $color,
         sprintf($format, ...$args),
-        Colors::RESET,
-        Colors::BG_BLACK,
+        Colors::RESET_ALL,
         PHP_EOL;  
 }
